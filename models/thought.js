da067bc3ef5fab108a1c5bb55ff6dcada4ae27b7
michael <mikek222@gmail.com>
2024-02-14 16:51:49 +0300

    completed user model, added index.js fopr models

--- models/thought.js
+++ models/thought.js
@@ -1,1 +1,25 @@
-const { Schema, model } = require ('mongoose')
+const { Schema, model } = require('mongoose')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+        reactions: {
+
+            // must be completed from other model
+
+        }
+    }
+)
---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, Model } = require('mongoose') 
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true, // Corrected from 'trimmed' to 'trim'
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User', 
+            },
+        ],
+    }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
