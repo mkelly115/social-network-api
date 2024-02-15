d408b9aa16f72d418a8b90b11f1d3898168c8de6
michael <mikek222@gmail.com>
2024-02-15 20:36:53 +0300

    updated and solidified the userRoute

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,17 +3,16 @@ const { User } = require('../../models');
 module.exports = {
     async getUsers(req, res) {
         try {
-            const users = await User.find({})
-                .select('-__v');
+            const users = await User.find({}).select('-__v');
             res.json(users);
         } catch (err) {
             res.status(500).json(err);
         }
     },
+
     async getSingleUser(req, res) {
         try {
-            const user = await User.findOne({ _id: req.params.userId })
-                .select('-__v');
+            const user = await User.findOne({ _id: req.params.userId }).select('-__v');
 
             !user
                 ? res.status(404).json({ message: 'No user is associated with that ID' })
@@ -22,17 +21,42 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
     async createUser(req, res) {
         try {
-            const user = User.create(
-                {
-                    "username": req.body,
-                    "email": req.body,
-                }
-            );
+            const user = await User.create({
+                username: req.body.username,
+                email: req.body.email,
+            });
+            res.json(user);
         } catch (err) {
-            console.log(err);
+            console.error(err);
             res.status(500).json(err);
         }
-    }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const user = await User.findByIdAndUpdate(
+                req.params.userId,
+                {
+                    username: req.body.username,
+                    email: req.body.email,
+                },
+                { new: true }
+            );
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.userId);
+            res.json(user);
+        } catch (err) {
+            res.status(404).json({ message: 'No user associated with this ID' });
+        }
+    },
 };
